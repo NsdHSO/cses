0f700267b73dbdf906982f1d8ccfc5b238bdca34
NsdHSO <nechiforelsamuel@gmail.com>
2026-09-15 06:24:22 +0300

    feat: 🎸 finish the weird_algor

--- src/bin/weird_algo.rs
+++ src/bin/weird_algo.rs
@@ -1,19 +1,17 @@
 use std::io;
 
-fn solve(mut n: i32) {
-    print!("{}", n);
+fn solve(mut n: i64) {
+    let mut out = String::new();
+    out.push_str(&format!("{} ", n));
     while n != 1 {
-        if (n % 2 == 0) {
-            n = n / 2;
-        } else {
-            n = n * 3 + 1;
-        }
-        print!(" {}", n)
+        n = if n % 2 == 0 { n / 2 } else { n * 3 + 1 };
+        out.push_str(&format!("{} ", n));
     }
+    print!("{}", out)
 }
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    let n: i32 = input.trim().parse().unwrap();
+    let n: i64 = input.trim().parse().unwrap();
     solve(n);
 }
